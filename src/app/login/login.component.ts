d8006e04f06b223d4b9551f2a1444d18b5f3fbc0
techcharm <techcharmindia@gmail.com>
2023-05-29 01:06:30 +0530

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,10 @@ import { MustMatch } from './password-match.validator';
 })
 export class LoginComponent implements OnInit {
   registerForm!: FormGroup;
+  loginForm!: FormGroup;
   submitted = false;
+  loginPassword = false;
+  toggleRegisterPage = false;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -19,17 +22,21 @@ export class LoginComponent implements OnInit {
           lastName: ['', Validators.required],
           email: ['', [Validators.required, Validators.email]],
           password: ['', [Validators.required, Validators.minLength(6)]],
-          confirmPassword: ['', Validators.required],
-          acceptTerms: [false, Validators.requiredTrue]
+          confirmPassword: ['', Validators.required]
       }, {
           validator: MustMatch('password', 'confirmPassword')
+      });
+
+      this.loginForm = this.formBuilder.group({
+          loginEmail: ['', [Validators.required, Validators.email]],
+          loginPassword: ['', [Validators.required]]
       });
   }
 
   // convenience getter for easy access to form fields
   get f() { return this.registerForm.controls; }
 
-  onSubmit() {
+  onRegisterSubmit() {
       this.submitted = true;
 
       // stop here if form is invalid
@@ -38,11 +45,23 @@ export class LoginComponent implements OnInit {
       }
 
       // display form values on success
-      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+      console.log('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  submitLoginForm(){
+    console.log(123,this.loginForm)
   }
 
   onReset() {
       this.submitted = false;
       this.registerForm.reset();
+  }
+
+  getLoginForm(){
+    this.loginPassword = !this.loginPassword
+  }
+
+  getToggleRegisterPage(){
+    this.toggleRegisterPage = !this.toggleRegisterPage
   }
 }
